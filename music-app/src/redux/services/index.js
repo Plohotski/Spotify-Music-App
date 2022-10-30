3ddf316b406e9c4b37078ad804165cba5498d1ce
Plohotski <plohoc@gmail.com>
2022-10-30 15:07:15 +0300

    fix: add error

--- music-app/src/redux/services/index.js
+++ music-app/src/redux/services/index.js
@@ -16,6 +16,7 @@ export const shazamCoreApi = createApi({
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
   }),
 });
 
@@ -26,4 +27,5 @@ export const {
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
   useGetSongsBySearchQuery,
+  useGetSongsByGenreQuery,
 } = shazamCoreApi;
